93cacb424771001b709843d3a33369126425f226
Cale Kennedy <me@calekennedy.com>
2017-03-10 15:57:14 -0800

    hello world

---
+++ hello_world/hello_world.pm
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+# set the version of perl
+use v5.24;
+
+# alternatively you can use this syntax with minor version
+# use 5.024;
+
+print "Hello, World!\n";
+
+# protip: don't forget to run the following command in your terminal window to make the file executable.
+
+# Run me with the following commands
+#   $ chmod a+x hello_world.pm
+# 1. $ ./hello_world.pm
+# 2. $ perl ./hello_world.pm
+# 3. $ perl hello_world.pm
